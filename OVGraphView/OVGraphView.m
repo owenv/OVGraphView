8a962bafcf822b171848c86307c3bfbbd8a65e5a
Owen Voorhees <owen@squibner.com>
2012-08-11 19:45:05 -0500

    changed structure to make OVGraphView a UIScrollView subclass which contains an OVGraphPlotView which does most of the drawing. The x and y axis now draw based on the size of the view

---
+++ OVGraphView/OVGraphView.m
@@ -0,0 +1,44 @@
+//
+//  OVGraphView.m
+//  OVGraphView
+//
+//  Created by Owen Voorhees on 8/11/12.
+//  Copyright (c) 2012 Owen Voorhees. All rights reserved.
+//
+
+#import "OVGraphView.h"
+
+@interface OVGraphView ()
+
+@end
+
+@implementation OVGraphView
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
